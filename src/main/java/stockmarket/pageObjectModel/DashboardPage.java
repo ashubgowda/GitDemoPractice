0cac5501063ab9e85d90e8a4052799e88c9250c1
ashwini <ashubgowda06@gmail.com>
2023-04-21 12:40:16 +0530

    add code to develop branch

--- src/main/java/stockmarket/pageObjectModel/DashboardPage.java
+++ src/main/java/stockmarket/pageObjectModel/DashboardPage.java
@@ -5,5 +5,10 @@ public class DashboardPage {
 	public void test() {
 		System.out.println("dashboard page");
 	}
-
+	public void test1() {
+		System.out.println("dashboard page1");
+	}
+	public void test2() {
+		System.out.println("dashboard page2");
+	}
 }
