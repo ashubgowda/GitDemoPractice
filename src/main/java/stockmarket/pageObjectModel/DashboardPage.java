9d548134c2e84538fcbfb34548b825da813429fe
ashwini <ashubgowda06@gmail.com>
2023-04-21 18:32:44 +0530

    gitignore file

--- src/main/java/stockmarket/pageObjectModel/DashboardPage.java
+++ src/main/java/stockmarket/pageObjectModel/DashboardPage.java
@@ -5,16 +5,5 @@ public class DashboardPage {
 	public void test() {
 		System.out.println("dashboard page");
 	}
-	public void test1() {
-		System.out.println("dashboard page1");
-	}
-	public void test2() {
-		System.out.println("dashboard page2");
-	}
-	public void test3() {
-		System.out.println("dashboard page3");
-	}
-	public void test4() {
-		System.out.println("dashboard page4");
-	}
+	
 }
